68107689cd34616c4ff4d803c454a81af62cad57
YuzukiTsuru <gloomyghost@gloomyghost.com>
2022-04-01 23:54:24 +0800

    add format

--- include/driver/chip/hal_adc.h
+++ include/driver/chip/hal_adc.h
@@ -45,130 +45,130 @@ extern "C" {
  * @brief ADC channel definition
  */
 typedef enum {
-	ADC_CHANNEL_0	= 0,
-	ADC_CHANNEL_1	= 1,
-	ADC_CHANNEL_2	= 2,
-	ADC_CHANNEL_3	= 3,
-	ADC_CHANNEL_4	= 4,
-	ADC_CHANNEL_5	= 5,
-	ADC_CHANNEL_6	= 6,
-	ADC_CHANNEL_7	= 7,
-	ADC_CHANNEL_8	= 8,
-	ADC_CHANNEL_NUM	= 9
+    ADC_CHANNEL_0 = 0,
+    ADC_CHANNEL_1 = 1,
+    ADC_CHANNEL_2 = 2,
+    ADC_CHANNEL_3 = 3,
+    ADC_CHANNEL_4 = 4,
+    ADC_CHANNEL_5 = 5,
+    ADC_CHANNEL_6 = 6,
+    ADC_CHANNEL_7 = 7,
+    ADC_CHANNEL_8 = 8,
+    ADC_CHANNEL_NUM = 9
 } ADC_Channel;
 
 /* ADC channel for VBAT voltage detection */
-#define ADC_CHANNEL_VBAT	ADC_CHANNEL_8
+#define ADC_CHANNEL_VBAT ADC_CHANNEL_8
 
 /**
  * @brief ADC register block structure
  */
 typedef struct {
-	__IO uint32_t SAMPLE_RATE;               /* offset: 0x00, GPADC sample rate configure register */
-	__IO uint32_t CTRL;                      /* offset: 0x04, GPADC control Register */
-	__IO uint32_t CMP_SEL_EN;                /* offset: 0x08, GPADC compare and select enable register */
-	__IO uint32_t FIFO_CTRL;                 /* offset: 0x0C, GPADC FIFO interrupt control register */
-	__IO uint32_t FIFO_STATUS;               /* offset: 0x10, GPADC FIFO interrupt status register */
-	__IO uint32_t FIFO_DATA;                 /* offset: 0x14, GPADC FIFO data register */
-	__IO uint32_t CALIB_DATA;                /* offset: 0x18, GPADC calibration data register */
-	__IO uint32_t RESERVED0;
-	__IO uint32_t LOW_CONFIG;                /* offset: 0x20, GPADC data low interrupt configure register */
-	__IO uint32_t HIGH_CONFIG;               /* offset: 0x24, GPADC data high interrupt configure register */
-	__IO uint32_t DATA_CONFIG;               /* offset: 0x28, GPADC data interrupt configure register */
-	__IO uint32_t RESERVED1;
-	__IO uint32_t LOW_STATUS;                /* offset: 0x30, GPADC data low interrupt status register */
-	__IO uint32_t HIGH_STATUS;               /* offset: 0x34, GPADC data high interrupt status register */
-	__IO uint32_t DATA_STATUS;               /* offset: 0x38, GPADC data interrupt status register */
-	__IO uint32_t RESERVED2;
-	__IO uint32_t CMP_DATA[ADC_CHANNEL_NUM]; /* offset: 0x40, GPADC CH n compare data register */
-	__IO uint32_t RESERVED3[7];
-	__I  uint32_t DATA[ADC_CHANNEL_NUM];     /* offset: 0x80, GPADC CH n data register */
+    __IO uint32_t SAMPLE_RATE; /* offset: 0x00, GPADC sample rate configure register */
+    __IO uint32_t CTRL;        /* offset: 0x04, GPADC control Register */
+    __IO uint32_t CMP_SEL_EN;  /* offset: 0x08, GPADC compare and select enable register */
+    __IO uint32_t FIFO_CTRL;   /* offset: 0x0C, GPADC FIFO interrupt control register */
+    __IO uint32_t FIFO_STATUS; /* offset: 0x10, GPADC FIFO interrupt status register */
+    __IO uint32_t FIFO_DATA;   /* offset: 0x14, GPADC FIFO data register */
+    __IO uint32_t CALIB_DATA;  /* offset: 0x18, GPADC calibration data register */
+    __IO uint32_t RESERVED0;
+    __IO uint32_t LOW_CONFIG;  /* offset: 0x20, GPADC data low interrupt configure register */
+    __IO uint32_t HIGH_CONFIG; /* offset: 0x24, GPADC data high interrupt configure register */
+    __IO uint32_t DATA_CONFIG; /* offset: 0x28, GPADC data interrupt configure register */
+    __IO uint32_t RESERVED1;
+    __IO uint32_t LOW_STATUS;  /* offset: 0x30, GPADC data low interrupt status register */
+    __IO uint32_t HIGH_STATUS; /* offset: 0x34, GPADC data high interrupt status register */
+    __IO uint32_t DATA_STATUS; /* offset: 0x38, GPADC data interrupt status register */
+    __IO uint32_t RESERVED2;
+    __IO uint32_t CMP_DATA[ADC_CHANNEL_NUM]; /* offset: 0x40, GPADC CH n compare data register */
+    __IO uint32_t RESERVED3[7];
+    __I uint32_t DATA[ADC_CHANNEL_NUM]; /* offset: 0x80, GPADC CH n data register */
 } ADC_T;
 
-#define ADC	((ADC_T *)GPADC_BASE) /* address: 0x40043000 */
+#define ADC (( ADC_T* )GPADC_BASE) /* address: 0x40043000 */
 
 /* ADC->SAMPLE_RATE */
-#define ADC_FS_DIV_SHIFT				16
-#define ADC_FS_DIV_MASK					(0xFFFFU << ADC_FS_DIV_SHIFT)
+#define ADC_FS_DIV_SHIFT 16
+#define ADC_FS_DIV_MASK (0xFFFFU << ADC_FS_DIV_SHIFT)
 
-#define ADC_TACQ_SHIFT					0
-#define ADC_TACQ_MASK					(0xFFFFU << ADC_TACQ_SHIFT)
+#define ADC_TACQ_SHIFT 0
+#define ADC_TACQ_MASK (0xFFFFU << ADC_TACQ_SHIFT)
 
 /* ADC->CTRL */
-#define ADC_FIRST_DELAY_SHIFT			24
-#define ADC_FIRST_DELAY_MASK			(0xFFU << ADC_FIRST_DELAY_SHIFT)
+#define ADC_FIRST_DELAY_SHIFT 24
+#define ADC_FIRST_DELAY_MASK (0xFFU << ADC_FIRST_DELAY_SHIFT)
 
-#define ADC_OP_BIAS_SHIFT				20
-#define ADC_OP_BIAS_MASK				(0x3U << ADC_OP_BIAS_SHIFT)
+#define ADC_OP_BIAS_SHIFT 20
+#define ADC_OP_BIAS_MASK (0x3U << ADC_OP_BIAS_SHIFT)
 
-#define ADC_WORK_MODE_SHIFT				18
-#define ADC_WORK_MODE_MASK				(0x3U << ADC_WORK_MODE_SHIFT)
+#define ADC_WORK_MODE_SHIFT 18
+#define ADC_WORK_MODE_MASK (0x3U << ADC_WORK_MODE_SHIFT)
 
 typedef enum {
-	ADC_SINGLE_CONV		= 0U,
-	ADC_SINGLE_CYCLE	= 1U,
-	ADC_CONTI_CONV		= 2U,
-	ADC_BURST_CONV		= 3U
+    ADC_SINGLE_CONV = 0U,
+    ADC_SINGLE_CYCLE = 1U,
+    ADC_CONTI_CONV = 2U,
+    ADC_BURST_CONV = 3U
 } ADC_WorkMode;
 
-#define ADC_CALIB_EN_BIT				HAL_BIT(17)
-#define ADC_EN_BIT						HAL_BIT(16)
-#define ADC_VBAT_EN_BIT					HAL_BIT(4)
-#define ADC_LDO_EN_BIT					HAL_BIT(0)
+#define ADC_CALIB_EN_BIT HAL_BIT(17)
+#define ADC_EN_BIT HAL_BIT(16)
+#define ADC_VBAT_EN_BIT HAL_BIT(4)
+#define ADC_LDO_EN_BIT HAL_BIT(0)
 
 /* ADC->CMP_SEL_EN */
-#define ADC_CMP_EN_SHIFT				16
-#define ADC_CMP_EN_MASK					(0x1FFU << ADC_CMP_EN_SHIFT)
+#define ADC_CMP_EN_SHIFT 16
+#define ADC_CMP_EN_MASK (0x1FFU << ADC_CMP_EN_SHIFT)
 
-#define ADC_SEL_EN_SHIFT				0
-#define ADC_SEL_EN_MASK					(0x1FFU << ADC_SEL_EN_SHIFT)
+#define ADC_SEL_EN_SHIFT 0
+#define ADC_SEL_EN_MASK (0x1FFU << ADC_SEL_EN_SHIFT)
 
 /*
  * ADC->LOW_CONFIG
  * ADC->HIGH_CONFIG
  * ADC->DATA_CONFIG
  */
-#define ADC_LOW_IRQ_MASK				0x1FFU
-#define ADC_HIGH_IRQ_MASK				0x1FFU
-#define ADC_DATA_IRQ_MASK				0x1FFU
+#define ADC_LOW_IRQ_MASK 0x1FFU
+#define ADC_HIGH_IRQ_MASK 0x1FFU
+#define ADC_DATA_IRQ_MASK 0x1FFU
 
 /*
  * ADC->FIFO_CTRL
  */
-#define ADC_FIFO_LEVEL_SHIFT			8
-#define ADC_FIFO_DATA_DRQ_MASK			HAL_BIT(18)
-#define ADC_FIFO_OVERUN_IRQ_MASK		HAL_BIT(17)
-#define ADC_FIFO_DATA_IRQ_MASK			HAL_BIT(16)
-#define ADC_FIFO_LEVEL_MASK				(0x3FU << ADC_FIFO_LEVEL_SHIFT)
-#define ADC_FIFO_FLUSH_MASK				HAL_BIT(4)
+#define ADC_FIFO_LEVEL_SHIFT 8
+#define ADC_FIFO_DATA_DRQ_MASK HAL_BIT(18)
+#define ADC_FIFO_OVERUN_IRQ_MASK HAL_BIT(17)
+#define ADC_FIFO_DATA_IRQ_MASK HAL_BIT(16)
+#define ADC_FIFO_LEVEL_MASK (0x3FU << ADC_FIFO_LEVEL_SHIFT)
+#define ADC_FIFO_FLUSH_MASK HAL_BIT(4)
 
 /*
  * ADC->FIFO_STATUS
  */
-#define ADC_FIFO_DATA_PENDING_MASK		HAL_BIT(16)
-#define ADC_FIFO_OVERUN_PENDING_MASK	HAL_BIT(17)
+#define ADC_FIFO_DATA_PENDING_MASK HAL_BIT(16)
+#define ADC_FIFO_OVERUN_PENDING_MASK HAL_BIT(17)
 
 /* ADC->FIFO_DATA */
-#define ADC_FIFO_DATA_MASK				0xFFFU
+#define ADC_FIFO_DATA_MASK 0xFFFU
 
 /*
  * ADC->LOW_STATUS
  * ADC->HIGH_STATUS
  * ADC->DATA_STATUS
  */
-#define ADC_LOW_PENDING_MASK			0x1FFU
-#define ADC_HIGH_PENDING_MASK			0x1FFU
-#define ADC_DATA_PENDING_MASK			0x1FFU
+#define ADC_LOW_PENDING_MASK 0x1FFU
+#define ADC_HIGH_PENDING_MASK 0x1FFU
+#define ADC_DATA_PENDING_MASK 0x1FFU
 
 /* ADC->CMP_DATA */
-#define ADC_HIGH_DATA_SHIFT				16
-#define ADC_HIGH_DATA_MASK				(0xFFFU << ADC_HIGH_DATA_SHIFT)
+#define ADC_HIGH_DATA_SHIFT 16
+#define ADC_HIGH_DATA_MASK (0xFFFU << ADC_HIGH_DATA_SHIFT)
 
-#define ADC_LOW_DATA_SHIFT				0
-#define ADC_LOW_DATA_MASK				(0xFFFU << ADC_LOW_DATA_SHIFT)
+#define ADC_LOW_DATA_SHIFT 0
+#define ADC_LOW_DATA_MASK (0xFFFU << ADC_LOW_DATA_SHIFT)
 
 /* ADC->DATA */
-#define ADC_DATA_MASK					0xFFFU
+#define ADC_DATA_MASK 0xFFFU
 
 /******************************************************************************/
 
@@ -176,54 +176,54 @@ typedef enum {
  * @brief ADC channel initialization parameters
  */
 typedef struct {
-	uint32_t		freq;	/* ADC sample frequency */
-	uint8_t			delay;	/* the number of delayed samples in first conversion */
-	ADC_WorkMode	mode;	/* ADC work mode */
+    uint32_t freq;     /* ADC sample frequency */
+    uint8_t delay;     /* the number of delayed samples in first conversion */
+    ADC_WorkMode mode; /* ADC work mode */
 } ADC_InitParam;
 
 /**
  * @brief ADC channel selected state definition
  */
 typedef enum {
-	ADC_SELECT_DISABLE	= 0,
-	ADC_SELECT_ENABLE	= 1
+    ADC_SELECT_DISABLE = 0,
+    ADC_SELECT_ENABLE = 1
 } ADC_Select;
 
 /**
  * @brief ADC interrupt mode definition
  */
 typedef enum {
-	ADC_IRQ_NONE			= 0,
-	ADC_IRQ_DATA			= 1,
-	ADC_IRQ_LOW				= 2,
-	ADC_IRQ_HIGH			= 3,
-	ADC_IRQ_LOW_DATA		= 4,
-	ADC_IRQ_HIGH_DATA		= 5,
-	ADC_IRQ_LOW_HIGH		= 6,
-	ADC_IRQ_LOW_HIGH_DATA	= 7
+    ADC_IRQ_NONE = 0,
+    ADC_IRQ_DATA = 1,
+    ADC_IRQ_LOW = 2,
+    ADC_IRQ_HIGH = 3,
+    ADC_IRQ_LOW_DATA = 4,
+    ADC_IRQ_HIGH_DATA = 5,
+    ADC_IRQ_LOW_HIGH = 6,
+    ADC_IRQ_LOW_HIGH_DATA = 7
 } ADC_IRQMode;
 
 /**
  * @brief ADC interrupt state definition
  */
 typedef enum {
-	ADC_NO_IRQ			= 0,
-	ADC_DATA_IRQ		= 1,
-	ADC_LOW_IRQ			= 2,
-	ADC_HIGH_IRQ		= 3,
-	ADC_LOW_DATA_IRQ	= 4,
-	ADC_HIGH_DATA_IRQ	= 5
+    ADC_NO_IRQ = 0,
+    ADC_DATA_IRQ = 1,
+    ADC_LOW_IRQ = 2,
+    ADC_HIGH_IRQ = 3,
+    ADC_LOW_DATA_IRQ = 4,
+    ADC_HIGH_DATA_IRQ = 5
 } ADC_IRQState;
 
 /** @brief Type define of ADC interrupt callback function */
-typedef void (*ADC_IRQCallback)(void *arg);
+typedef void (*ADC_IRQCallback)(void* arg);
 
-HAL_Status HAL_ADC_Init(ADC_InitParam *initParam);
+HAL_Status HAL_ADC_Init(ADC_InitParam* initParam);
 HAL_Status HAL_ADC_DeInit(void);
 
-HAL_Status HAL_ADC_Conv_Polling(ADC_Channel chan, uint32_t *data, uint32_t msec);
+HAL_Status HAL_ADC_Conv_Polling(ADC_Channel chan, uint32_t* data, uint32_t msec);
 
-HAL_Status HAL_ADC_EnableIRQCallback(ADC_Channel chan, ADC_IRQCallback cb, void *arg);
+HAL_Status HAL_ADC_EnableIRQCallback(ADC_Channel chan, ADC_IRQCallback cb, void* arg);
 HAL_Status HAL_ADC_DisableIRQCallback(ADC_Channel chan);
 
 HAL_Status HAL_ADC_ConfigChannel(ADC_Channel chan, ADC_Select select, ADC_IRQMode mode, uint32_t lowValue, uint32_t highValue);
--- include/driver/chip/hal_audio.h
+++ include/driver/chip/hal_audio.h
@@ -36,26 +36,25 @@
 #define HAL_AUDIO_H_H
 
 typedef enum {
-	DAIFMT_CBM_CFM,			/*(codec clk & FRM master)        use((i2s slave))*/
-	DAIFMT_CBS_CFM,			/*(codec clk slave & FRM master)  not use*/
-	DAIFMT_CBM_CFS,			/*(codec clk master & frame slave) not use*/
-	DAIFMT_CBS_CFS,			/*(codec clk & FRM slave)         use(i2s master)*/
+    DAIFMT_CBM_CFM, /*(codec clk & FRM master)        use((i2s slave))*/
+    DAIFMT_CBS_CFM, /*(codec clk slave & FRM master)  not use*/
+    DAIFMT_CBM_CFS, /*(codec clk master & frame slave) not use*/
+    DAIFMT_CBS_CFS, /*(codec clk & FRM slave)         use(i2s master)*/
 } PCM_ClkMode;
 
 typedef enum {
-	DAIFMT_I2S,			/*(standard i2s format).    use*/
-	DAIFMT_RIGHT_J,			/*(right justfied format).*/
-	DAIFMT_LEFT_J,			/*(left justfied format) */
-	DAIFMT_DSP_A,			/*(pcm. MSB is available on 2nd BCLK rising edge after LRC rising edge). use*/
-	DAIFMT_DSP_B,			/*(pcm. MSB is available on 1nd BCLK rising edge after LRC rising*/
+    DAIFMT_I2S,     /*(standard i2s format).    use*/
+    DAIFMT_RIGHT_J, /*(right justfied format).*/
+    DAIFMT_LEFT_J,  /*(left justfied format) */
+    DAIFMT_DSP_A,   /*(pcm. MSB is available on 2nd BCLK rising edge after LRC rising edge). use*/
+    DAIFMT_DSP_B,   /*(pcm. MSB is available on 1nd BCLK rising edge after LRC rising*/
 } PCM_TranFmt;
 
 typedef enum {
-	DAIFMT_NB_NF,			/*(normal bit clock + frame)  use*/
-	DAIFMT_NB_IF,			/*(normal BCLK + inv FRM) */
-	DAIFMT_IB_NF,			/*(invert BCLK + nor FRM)  use*/
-	DAIFMT_IB_IF,			/*(invert BCLK + FRM)*/
+    DAIFMT_NB_NF, /*(normal bit clock + frame)  use*/
+    DAIFMT_NB_IF, /*(normal BCLK + inv FRM) */
+    DAIFMT_IB_NF, /*(invert BCLK + nor FRM)  use*/
+    DAIFMT_IB_IF, /*(invert BCLK + FRM)*/
 } PCM_SignalInv;
-
 
 #endif
